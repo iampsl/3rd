7222a6e7d33b96a37e306c221c3f95f13d99987c
iampsl <iampsl@qq.com>
2017-09-01 13:34:48 +0800

    新写的一个日志类

--- mysrc/server/src/MyLog.h
+++ mysrc/server/src/MyLog.h
@@ -1,80 +1,246 @@
-#ifndef PSL_MY_LOG_H
-#define PSL_MY_LOG_H
+#ifndef PSL_MYLOG_H
+#define PSL_MYLOG_H
 
 #include <stdio.h>
 #include <time.h>
-#include <deque>
-#include <vector>
-#include <memory>
-#include <thread>
+#include <inttypes.h>
+#include <string>
+#include <sstream>
+#include <stdexcept>
 #include <mutex>
+#include <type_traits>
 
-#define LOG_INFO 0
-#define LOG_TOTAL 1
+#ifdef MY_LOG_A
+#error "macro have already defined"
+#endif
 
-int get_mmddhhmmss(const time_t & t);
+#ifdef MY_LOG_B
+#error "macro have already defined"
+#endif
+
+#ifdef MY_LOG_OP
+#error "macro have already defined"
+#endif
+
+#ifdef MYLOG
+#error "macro have already defined"
+#endif
+
+#define LOGERROR 0x01
+#define LOGINFO 0x02
 
 class MyLog
 {
 public:
-	struct message_data
+	MyLog():MY_LOG_A(*this), MY_LOG_B(*this)
 	{
-		uint8_t buffer[512];
-		unsigned int size;
+		m_level = static_cast<unsigned int>(-1);
+		m_file = nullptr;
+		m_maxSize = 1024 * 1024 * 10;
+	}
+	void init(const char * filePrefix, const char * fileSuffix, const char * fileDir, size_t maxSize)
+	{
+		if (nullptr != filePrefix)
+		{
+			m_filePrefix.assign(filePrefix);
+		}
+		if (nullptr != fileSuffix)
+		{
+			m_fileSuffix.assign(fileSuffix);
+		}
+		if (nullptr != fileDir)
+		{
+			m_fileDir.assign(fileDir);
+			if (!m_fileDir.empty())
+			{
+				if (m_fileDir.back() != '/')
+				{
+					m_fileDir.push_back('/');
+				}
+			}
+		}
+		if (maxSize > (1024 * 1024))
+		{
+			m_maxSize = maxSize;
+		}
+	}
+	inline unsigned int getLevel()
+	{
+		return m_level;
+	}
+	void setLevel(unsigned int level)
+	{
+		m_level = level;
+	}
+	MyLog & capture(const char * file, int line, int level)
+	{
+		time_t now;
+		time(&now);
+		if (m_file)
+		{
+			if (m_curSize >= m_maxSize)
+			{
+				fflush(m_file);
+				fclose(m_file);
+				m_file = nullptr;
+			}
+		}
+		if (nullptr == m_file)
+		{
+			m_ostrstream.str("");
+			time(&m_createTime);
+			m_curSize = 0;
+			m_ostrstream << m_fileDir << m_filePrefix << timeToInt(m_createTime) << m_fileSuffix;
+			
+#ifdef PSL_WINDOWS
+			m_file = _fsopen(m_ostrstream.str().c_str(), "a", _SH_DENYWR);
+#else
+			m_file = fopen(m_ostrstream.str().c_str(), "a");
+#endif
+		}
+		if (nullptr == m_file)
+		{
+			return *this;
+		}
+		m_ostrstream.str("");
+		m_ostrstream << "[" << timeToInt(now) << "]" << "[" << getLevelStr(level) << "]" << "[" << file << ":" << line << "]";
+		std::string info = m_ostrstream.str();
+		size_t iwrite = fwrite(&(info[0]), info.size(), 1, m_file);
+		if (1 == iwrite)
+		{
+			m_curSize += info.size();
+		}
+		else
+		{
+			fflush(m_file);
+			fclose(m_file);
+			m_file = nullptr;
+		}
+		return *this;
+	}
+	template<typename T>
+	MyLog & capture(const char * name, const T & value)
+	{
+		if (nullptr == m_file)
+		{
+			return *this;
+		}
+		m_ostrstream.str("");
+		m_ostrstream << "{" << name << ":" << value << "}";
+		std::string info = m_ostrstream.str();
+		size_t iwrite = fwrite(&(info[0]), info.size(), 1, m_file);
+		if (1 == iwrite)
+		{
+			m_curSize += info.size();
+		}
+		else
+		{
+			fflush(m_file);
+			fclose(m_file);
+			m_file = nullptr;
+		}
+		return *this;
+	}
+	~MyLog()
+	{
+		if (m_file)
+		{
+			fflush(m_file);
+			fclose(m_file);
+			m_file = nullptr;
+		}
+	}
+	class lockguard
+	{
+	public:
+		lockguard(MyLog & ml):m_log(ml)
+		{
+			m_log.getMutex().lock();
+		}
+		~lockguard()
+		{
+			m_log.newLine();
+			m_log.getMutex().unlock();
+		}
+		operator bool()
+		{
+			return true;
+		}
+	private:
+		lockguard(const lockguard &) = delete;
+		lockguard & operator =(const lockguard&) = delete;
+	private:
+		MyLog & m_log;
 	};
 public:
-	static MyLog & getInstance();
-	void setLogInfo(int whichlog, const char * pfile_prefix, const char * pfile_suffix, const char * pfile_dir, size_t max_size);
-	void start();
-	void outputMsg(int whichlog, const message_data & msg);
-	void stop();
-private:
-	void work_thread();
-	static void gen_file_path(void * pout, size_t out_size, const char * pfile_dir, const char * pfile_prefix, const char * pfile_suffix, const time_t & t);
-private:
-	MyLog();
-	MyLog(const MyLog & other) = delete;
-	MyLog & operator = (const MyLog & other) = delete;
-	~MyLog();
-private:
-	struct log_info 
+	MyLog & MY_LOG_A;
+	MyLog & MY_LOG_B;
+protected:
+	std::mutex & getMutex()
 	{
-		char file_name_prefix[64];
-		char file_name_suffix[64];
-		char file_save_directory[1024];
-		size_t file_max_size;
-		size_t file_cur_size;
-		time_t create_time;
-		FILE * pfile;
-		std::deque<message_data> messages[2];
-		size_t cur_worker_index;
-		std::mutex messages_mutex;
-		bool bstart;
-		size_t cur_insert_index;
-	};
-	static_assert(LOG_TOTAL != 0, "");
+		return m_mutex;
+	}
+	void newLine()
+	{
+		if (m_file)
+		{
+			char nline[] = "\n";
+			size_t iwrite = fwrite(nline, sizeof(nline) - 1, 1, m_file);
+			if (iwrite == 1)
+			{
+				m_curSize += (sizeof(nline) - 1);
+				fflush(m_file);
+			}
+			else
+			{
+				fflush(m_file);
+				fclose(m_file);
+				m_file = nullptr;
+			}
+		}
+	}
+	uint64_t timeToInt(const time_t & t)
+	{
+		tm tout;
+#ifdef PSL_WINDOWS
+		localtime_s(&tout, &t);
+#else
+		localtime_r(&t, &tout);
+#endif
+		uint64_t ret = static_cast<uint64_t>(tout.tm_year + 1900)*10000000000 + static_cast<uint64_t>(tout.tm_mon + 1) * 100000000 + static_cast<uint64_t>(tout.tm_mday) * 1000000 + static_cast<uint64_t>(tout.tm_hour) * 10000 + static_cast<uint64_t>(tout.tm_min) * 100 + static_cast<uint64_t>(tout.tm_sec);
+		return ret;
+	}
+	const char * getLevelStr(int level)
+	{
+		switch (level)
+		{
+		case LOGERROR:
+			return "ERROR";
+		case LOGINFO:
+			return "INFO";
+		default:
+			return "NULL";
+		}
+	}
 private:
-		log_info m_logs[LOG_TOTAL];
-		std::shared_ptr<std::thread>  m_pthread;
+	MyLog(const MyLog &) = delete;
+	MyLog & operator = (const MyLog &) = delete;
+private:
+	std::mutex m_mutex;
+	unsigned int m_level;
+	std::ostringstream m_ostrstream;
+	FILE * m_file;
+	std::string m_filePrefix;
+	std::string m_fileSuffix;
+	std::string m_fileDir;
+	size_t m_maxSize;
+	size_t m_curSize;
+	time_t m_createTime;
 };
 
-
-
-#define  MYLOG(which_log, format_string, ...) \
-{\
-    MyLog::message_data msg; \
-    msg.size = (unsigned)(sizeof(msg.buffer)-1); \
-    int iprintf = snprintf((char*)(msg.buffer), sizeof(msg.buffer), format_string, ##__VA_ARGS__); \
-    if (iprintf > 0)\
-    {\
-        if ((size_t)iprintf < sizeof(msg.buffer))\
-        {\
-            msg.size = (unsigned)iprintf; \
-        }\
-        MyLog::getInstance().outputMsg(which_log, msg); \
-    }\
-}
-
+#define MY_LOG_A(x) MY_LOG_OP(x,B)
+#define MY_LOG_B(x) MY_LOG_OP(x,A)
+#define MY_LOG_OP(x,next) MY_LOG_A.capture((#x),(x)).MY_LOG_##next
+#define MYLOG(expr1,expr2) static_assert(std::is_same<decltype(expr1), MyLog>::value, #expr1" is not MyLog object"); static_assert(((expr2)==LOGERROR || (expr2)== LOGINFO), #expr2" is not defined"); if((expr2)&(expr1).getLevel()) if(MyLog::lockguard tmp = (expr1)) (expr1).capture(__FILE__, __LINE__, (expr2)).MY_LOG_A
 
 #endif
-
